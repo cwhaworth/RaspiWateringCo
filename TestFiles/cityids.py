210469ebc96e75cb686dd11d8056fe5b1c9e95fa
Chris Haworth <46871314+cwhaworth@users.noreply.github.com>
2021-05-26 22:52:50 -0400

    Delete cityids.py

--- TestFiles/cityids.py
+++
@@ -1,6 +0,0 @@
-#testing file, does not currently work
-from pyowm.commons import cityids
-from pyowm.commons import cityidregistry
-
-raleigh = cityidregistry.ids_for('raleigh', country='US', matching='nocase')
-print(str(raleigh))
