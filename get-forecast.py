33b81fea8ae8d2f08a33013c26a5921ed7af6c9e
Chris Haworth <cwhaworth@gmail.com>
2021-09-25 17:32:01 -0400

    edit get-forecast.py

--- get-forecast.py
+++ get-forecast.py
@@ -1,7 +1,7 @@
 from pyowm.owm import OWM
 
 #api key
-owm = OWM('YOUR-API-KEY-HERE')
+owm = OWM('1c29d386be313a4c1bc91710a3ff6d01')
 
 #api call
 mgr = owm.weather_manager()
@@ -12,6 +12,6 @@ log = open("forecast.txt", "w")
 
 #write forecast from api call to log
 for weather in fcast:
-	log.write("Forecast: " + str(weather) + "\n")
+	log.write(str(weather) + "\n")
 
 log.close()
